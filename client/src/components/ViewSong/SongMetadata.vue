40ac0397476bc30e5d9ab5fa4e7db3d833f5ef8f
Manuel Hintermayr <manuel@hintermayr.net>
2025-04-06 20:06:44 +0200

    Updated SUT to a newer version

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -2,57 +2,40 @@
   <panel title="Song Metadata">
     <v-layout>
       <v-flex xs6>
-        <div class="song-title">
-          {{song.title}}
+        <div class="song-title" data-test-id="songTitle">
+          {{ song.title }}
         </div>
-        <div class="song-artist">
-          {{song.artist}}
+        <div class="song-artist" data-test-id="songArtist">
+          {{ song.artist }}
         </div>
         <div class="song-genre">
-          {{song.genre}}
+          {{ song.genre }}
         </div>
 
         <v-btn
           dark
           class="cyan"
           :to="{
-            name: 'song-edit', 
-            params () {
+            name: 'song-edit',
+            params() {
               return {
-                songId: song.id
+                songId: song.id,
               }
-            }
-          }">
+            },
+          }"
+        >
           Edit
         </v-btn>
 
-        <v-btn
-          v-if="isUserLoggedIn && !bookmark"
-          dark
-          class="cyan"
-          @click="setAsBookmark">
-          Set As Bookmark
-        </v-btn>
+        <v-btn v-if="isUserLoggedIn && !bookmark" dark class="cyan" @click="setAsBookmark"> Set As Bookmark </v-btn>
 
-        <v-btn
-          v-if="isUserLoggedIn && bookmark"
-          dark
-          class="cyan"
-          @click="unsetAsBookmark">
-          Unset As Bookmark
-        </v-btn>
+        <v-btn v-if="isUserLoggedIn && bookmark" dark class="cyan" @click="unsetAsBookmark"> Unset As Bookmark </v-btn>
       </v-flex>
 
       <v-flex xs6>
         <img class="album-image" :src="song.albumImageUrl" />
-        <br>
-        <v-btn
-          dark
-          class="cyan"
-          id="fileDownload"
-          @click="fileDownload">
-          Download Image
-        </v-btn>
+        <br />
+        <v-btn dark class="cyan" id="fileDownload" @click="fileDownload"> Download Image </v-btn>
         <!-- <br>
         {{song.album}} -->
       </v-flex>
@@ -61,53 +44,52 @@
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from 'vuex'
 import BookmarksService from '@/services/BookmarksService'
 
 export default {
-  props: [
-    'song'
-  ],
-  data () {
+  props: ['song'],
+  data() {
     return {
-      bookmark: null
+      bookmark: null,
     }
   },
   computed: {
-    ...mapState([
-      'isUserLoggedIn',
-      'user'
-    ])
+    ...mapState(['isUserLoggedIn', 'user']),
   },
   watch: {
-    async song () {
+    async song() {
       if (!this.isUserLoggedIn) {
         return
       }
 
       try {
-        const bookmarks = (await BookmarksService.index({
-          songId: this.song.id
-        })).data
+        const bookmarks = (
+          await BookmarksService.index({
+            songId: this.song.id,
+          })
+        ).data
         if (bookmarks.length) {
           this.bookmark = bookmarks[0]
         }
       } catch (err) {
         console.log(err)
       }
-    }
+    },
   },
   methods: {
-    async setAsBookmark () {
+    async setAsBookmark() {
       try {
-        this.bookmark = (await BookmarksService.post({
-          songId: this.song.id
-        })).data
+        this.bookmark = (
+          await BookmarksService.post({
+            songId: this.song.id,
+          })
+        ).data
       } catch (err) {
         console.log(err)
       }
     },
-    async unsetAsBookmark () {
+    async unsetAsBookmark() {
       try {
         await BookmarksService.delete(this.bookmark.id)
         this.bookmark = null
@@ -115,24 +97,24 @@ export default {
         console.log(err)
       }
     },
-    async fileDownload () {
+    async fileDownload() {
       axios({
-            url: this.song.albumImageUrl,
-            method: 'GET',
-            responseType: 'blob',
-        }).then((response) => {
-              var fileURL = window.URL.createObjectURL(new Blob([response.data]));
-              var fileLink = document.createElement('a');
+        url: this.song.albumImageUrl,
+        method: 'GET',
+        responseType: 'blob',
+      }).then(response => {
+        var fileURL = window.URL.createObjectURL(new Blob([response.data]))
+        var fileLink = document.createElement('a')
 
-              fileLink.href = fileURL;
-              fileLink.setAttribute('download', 'file.jpg');
-              document.body.appendChild(fileLink);
+        fileLink.href = fileURL
+        fileLink.setAttribute('download', 'file.jpg')
+        document.body.appendChild(fileLink)
 
-              fileLink.click();
-        });
-  }
+        fileLink.click()
+      })
+    },
+  },
 }
-  }
 //}
 </script>
 
